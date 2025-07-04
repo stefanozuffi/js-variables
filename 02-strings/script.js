0da316b8672afe1b6f4d37e41d685f567138f550
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-04 11:54:28 +0200

    Exercise 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,23 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+const name = 'Stefano';
 // Stampa il valore di nome nella console.
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+let number
+let text
+let bool
 // un numero, una stringa e un valore booleano.
+number = 66
+text = 'Truceklan'
+bool = true
 // Stampa tutti i valori nella console.
+//console.log(number, text, bool)
+console.log(number)
+console.log(text)
+console.log(bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +26,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + ' ' + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
