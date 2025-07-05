324a33439003e7244a3a336166e4dc500814c47b
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-05 09:38:46 +0200

    extra string bonus

--- extra-in-class/script.js
+++ extra-in-class/script.js
@@ -39,26 +39,26 @@ const welcome = 'welcome'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere 'o' nella variabile region
-const regionIndex = 0;
+const regionIndex = region.indexOf('o');
 console.log(regionIndex); // Output: 1
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "yo" dalla variabile region
-const regionSubstring = '';
+const regionSubstring = region.substring(3,);
 console.log(regionSubstring); // Output: "yo"
 
 // 3. Sostituzione
 // Sostituisci 'Tokyo' con 'Osaka'
-const regionReplaced = '';
+const regionReplaced = region.replace('Tokyo', 'Osaka');
 console.log(regionReplaced); // Output: "Osaka"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi regionNameConcat uguale a 'Tokyo Region'
-const regionNameConcat = '';
+const regionNameConcat = `${region} ${regionName}`;
 console.log(regionNameConcat); // Output: "Tokyo Region"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiuscolo la stringa della variabile welcome
-const upperCaseWelcome = '';
+const upperCaseWelcome = welcome.toUpperCase();
 console.log(upperCaseWelcome); // Output: "WELCOME"
